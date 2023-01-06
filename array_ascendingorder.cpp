85ea48b728b61d563d51ddee408d933febeaf480
Shreya <111882798+shreya3106@users.noreply.github.com>
2023-01-06 15:11:14 +0530

    Add files via upload

---
+++ array_ascendingorder.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i, j, size, temp;
+    int arr[25];
+    
+    cout << "Enter the total no. of elements: ";
+    cin >> size;
+
+    cout << "Enter the elements of the array: " << endl;
+    for (i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+    for (i = 0; i < size; i++){
+        for (j = i; j < size; j++){
+            if (arr[i] > arr[j+1]){
+                temp = arr[i];
+                arr[i] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+   
+    cout << "Elements sorted in the ascending order are: " << endl;
+    for (i = 1; i <= size; i++){
+     cout << arr[i] <<" ";   
+    }
+    return 0;
+}
